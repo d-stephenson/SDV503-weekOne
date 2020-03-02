d0325a4e9542b218e2c34aeb412ac6a38ab9f6f0
Dale <dalestephenson@protonmail.com>
2020-03-03 12:24:58 +1300

    Ali we have done it please check it out
    BOOM

---
+++ index.js
@@ -0,0 +1,26 @@
+/*var numOne = 1, numTwo = 2, numThree = 3, numFour = 4, numFive = 5, numSix = 6, numSeven = 7, numEight = 8, numNine = 9, numZero = 0;
+
+sum = (numNine + (numThree - numOne) + numThree)
+
+console.log("sum = " + sum)*/
+
+
+/*var globalVariable = 'I am global'
+const globalConstVariable = 'I am also global'
+
+function myFunc() {
+    var myLocalVariable = 'I am local'
+    let myLocalLetVariable = 'I am also local'  
+    console.log(myLocalVariable)
+}
+myFunc()*/
+
+function myFunc() {
+    var myLocalVariable = 'I am local'
+    let myLocalLetVariable = 'I am also local'  
+    function myInnerFunc() {
+        console.log(myLocalVariable)
+    }
+    return myInnerFunc()
+  }
+myFunc()

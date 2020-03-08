746b2e3fd2fef4c43e69df53d88e595f8e6a7754
Dale Stephenson <dalestephenson@zohomail.com>
2020-03-08 15:28:07 +1300

    update

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Hello World")
+
+var firstName;
+firstName = "Dale"
+console.log(firstName)
+
+var lastName = "Stephenson"
+console.log(firstName + ' ' + lastName)
+
+    var a = 10;
+    var b = 25;
+    var sum = a * b;
+    console.log(sum);
+
+    var a = 20, b = 35, sum = a + b;
+    console.log(sum);
+
+    var a, b, sum;
+    a = 30;
+    b = 45;
+    sum = a + b;
+    console.log(sum);
